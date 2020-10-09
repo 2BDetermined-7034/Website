e3893bcc2e6d64b5ba2c4d6deb1e6776d374a1a1
sr64 <66576546+sr64@users.noreply.github.com>
2020-10-08 18:56:08 -0700

    Update Calendar.js

--- website/src/pages/Calendar.js
+++ website/src/pages/Calendar.js
@@ -8,7 +8,7 @@ class Calendar extends Component {
                 <div className="jumbotron text-center bg-white">
                     <h1 className="display-4 font-weight-bold">Meeting Schedule</h1>
                     <p className="lead">
-                        Team Meetings: Mondays and Thursdays at 3:30 PM on Zoom
+                        
                     </p>
                     <hr className="my-4" />
                     <iframe title="calendar" className="mb-2" src="https://calendar.google.com/calendar/embed?src=wlhsfrc%40gmail.com&ctz=America%2FLos_Angeles" frameBorder="0" width="100%" height="700" scrolling="no"/>
