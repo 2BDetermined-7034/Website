298413db39320a4b7f82bcc74510c65c7a8ff836
sabrina <blackforest070322@gmail.com>
2021-12-07 20:33:11 -0800

    some changes to make the webpage more beginner friendly

--- website/src/pages/HourOfCode.js
+++ website/src/pages/HourOfCode.js
@@ -24,12 +24,48 @@ class HourOfCode extends Component {
                 </div>
                 <img className="coding-img" src={require("../artifacts/images/Code typing-bro.png")} alt="coding-img"></img>
                 <div className="content">
-                    <div className="second-title"><h2>A great way to get started your coding journey~</h2></div>
+                    <div className="second-title"><h2>A great way to start your coding journey~</h2></div>
                     <div className = "dottedLine"></div>
-                        <div className="resources">
-                            <h2>Resources.</h2>
-                            <p>Below are some amazing free online resources that can teach you about Python, Java, HTML, CSS, and Javascript</p>
-                            <p>happy coding everyone 🖤💗</p>
+                        <div className="intro">
+                            <h2 className = "h2">Introduction</h2>
+                            <img className = "intro-img"
+                            src = {require("../artifacts/images/Code review-amico.png")}
+                            alt = "intro-img"
+                            />
+                            <p className = "body">Programming, or the field of Computer Science, <br />
+                             has shaped the world with fascinating
+                                invention. <br /> From building a "Would you rather" to facial recognition, <br />Programming has
+                                become a valuable skill to gain in our generation and beyond. <br />
+                            if you want to explore deeper
+                            of how computer science is changing the world, <br/>
+                            watch this video ✈✈✈
+                            <a className="intro-vid" href="https://youtu.be/xJqSu1IbcHg?list=PLzdnOPI1iJNfpD8i4Sx7U0y2MccnrNZuP">here :))</a>
+                            </p>
+                        </div>
+                        <div className = "dottedLine"></div>
+                        <div>
+                            <img className = "start-off-img"
+                            src = {require("../artifacts/images/Source code-bro.png")}
+                            alt = "start-off-img"
+                            />
+                            <h2 className = "h2">To Start Off...</h2>
+                            <p className="body">If you are a complete beginner and have no idea <br />
+                            how programming or programming 
+                                language works, <br />
+                                no worries! Let's start this journey with 
+                                <a className="programming-vid" href="https://youtu.be/EGQh5SZctaE" >this video! </a><br />
+                                There are many unique programming languages, <br />
+                                with all of them have its own specialty! <br />
+                                If you are wondering what lanaguage you want to learn first, <br/>
+                                then scroll down to the Resources tab to see some of the <br />
+                                most widely use and beginner friendly programming language!
+                            </p>
+                        </div>
+                        <div className = "dottedLine"></div>
+                        <div>
+                            <h2 className="h2">Resources.</h2>
+                            <p className="body">Below are some amazing free online resources that can teach you about Python, Java, HTML, CSS, and Javascript</p>
+                            <p className="body">Happy coding everyone 🖤💗</p>
                         </div>
 
                         <div className = "dottedLine"></div>
@@ -40,10 +76,9 @@ class HourOfCode extends Component {
                                 />
                                 <ul className="python-coding-resources">
                                     <a className="mosh-python" href="https://youtu.be/kqtD5dpn9C8">Programming with Mosh - 🐍</a><br/>
-                                    <a className="w3schools" href="https://www.w3schools.com/python/default.asp">W3Schools - 🐍</a><br/>
-                                    <a className="learnPython" href="https://www.learnpython.org/">LearnPython - 🐍</a><br/>
-                                    <a className="A-Byte-Of-Python" href="https://python.swaroopch.com/">A Byte of Python - 🐍</a><br/>
-                                    <a className="google-python" href="https://developers.google.com/edu/python">Python with Google - 🐍</a><br/>
+                                    <p className="python-descr">In this 1 hour python completely beginner tutorial, Mosh will teach you step-by-step of 
+                                    how to set up a python environment,
+                                    code a simple project, and walk through many cool concepts such as in Python!</p>
                                 </ul>
                             </div>
 
@@ -53,11 +88,10 @@ class HourOfCode extends Component {
                                 alt="java-logo"
                                 />
                                 <ul className="java-coding-resources">
-                                    <a className="mosh-java" href="https://youtu.be/eIrMbAQSU34">Programming with Mosh - ☕</a><br/>
-                                    <a className="w3schools" href="https://www.w3schools.com/java/default.asp">W3Schools - ☕</a><br/>
                                     <a className="geeksforgeeks" href="https://www.geeksforgeeks.org/java/?ref=shm">GeeksforGeeks - ☕</a><br/>
-                                    <a className="codingbat" href="https://codingbat.com/java">CodingBat - ☕</a><br/>
-                                    <a className="exploreJava" href="https://developer.ibm.com/learningpaths/java-get-started/">Explore Java - ☕</a><br/>
+                                    <p className="java-descr">This self-paced Java tutorial goes in detail of 
+                                    what Java is and how Java works, including concepts such as Strings,
+                                    Methods, Flow Control etc that will allow you to code a cool program of your own!</p>
                                 </ul>
                             </div>
 
@@ -67,11 +101,10 @@ class HourOfCode extends Component {
                                 alt="the-combo-logo"
                                 />
                                 <ul className="the-combo-resources">
-                                    <a className="mosh-html" href="https://youtu.be/qz0aGYrrlhU">Programming with Mosh HTML - 📑</a><br/>
-                                    <a className="mosh-javascript" href="https://youtu.be/W6NZfCO5SIk">Programming with Mosh Javascript - 📑</a><br/>
-                                    <a className="w3school-css" href="https://www.w3schools.com/css/">W3Schools CSS - 📑</a><br/>
-                                    <a className="w3school-javascript" href="https://www.w3schools.com/js/default.asp">W3Schools Javascript - 📑</a><br/>
-                                    <a className="w3school-html" href="https://www.w3schools.com/html/default.asp">W3Schools HTML - 📑</a><br/>
+                                    <a className="w3school-css" href="https://www.w3schools.com">W3Schools - 📑</a><br/>
+                                    <p className="combo-dev">W3Schools include lots of fun practices along with detailed
+                                    description for HTML, CSS, and Javascript, the combo you would need to build
+                                    a functional website, like this one!</p>
 
                                 </ul>
                             </div>
